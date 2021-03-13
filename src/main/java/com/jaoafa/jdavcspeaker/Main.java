28ade1619a36e440a719f0f0e4e8a49a4d8cdac9
yuuaHP <yuuahp@icloud.com>
2021-03-13 23:45:44 +0900

    MainがStaticんあってなかった

--- src/main/java/com/jaoafa/jdavcspeaker/Main.java
+++ src/main/java/com/jaoafa/jdavcspeaker/Main.java
@@ -6,7 +6,7 @@ import com.jaoafa.jdavcspeaker.Util.Logger;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
-    public void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception{
         Logger.print("VCSpeaker Starting...");
         JDABuilder builder = JDABuilder.createDefault(JSONUtil.read("./VCSpeaker.json").getString("DiscordToken"));
         builder.addEventListeners(new CmdHook());
