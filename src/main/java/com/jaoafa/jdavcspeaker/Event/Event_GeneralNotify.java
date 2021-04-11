7fadd67cb2eaca4f559a4c73e452579a6840a524
yuuaHP <yuuahp@icloud.com>
2021-04-11 11:07:26 +0900

    feat: generalnotify

---
+++ src/main/java/com/jaoafa/jdavcspeaker/Event/Event_GeneralNotify.java
@@ -0,0 +1,31 @@
+package com.jaoafa.jdavcspeaker.Event;
+
+import com.jaoafa.jdavcspeaker.Lib.VoiceText;
+import com.jaoafa.jdavcspeaker.StaticData;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.hooks.SubscribeEvent;
+
+public class Event_GeneralNotify {
+    @SubscribeEvent
+    public void onMemberJoin(GuildVoiceJoinEvent event) {
+        if (event.getMember().getUser().isBot()) {
+            return;
+        }
+        if (!event.getGuild().getId().equals("597378876556967936")){
+            return;
+        }
+        if (event.getChannelJoined().getMembers().size() >= 2&&event.getGuild().getSelfMember().getVoiceState().getChannel() != event.getChannelJoined()){
+            return;
+        }
+        if (event.getChannelJoined().getMembers().size() != 1){
+            return;
+        }
+
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle(":inbox_tray: 会話が始まりました！");
+        eb.setDescription(event.getMember().getAsMention()+String.format("が`%s`に参加しました。",event.getChannelJoined().getName()) +
+                String.format("\n(クリックして参加)[%s]",event.getChannelJoined().createInvite().setMaxAge(3600).complete().getUrl()));
+        event.getJDA().getTextChannelById("597419057251090443").sendMessage(eb.build()).queue();
+    }
+}
