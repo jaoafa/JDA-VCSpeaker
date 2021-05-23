c7f64fd6c30e8d614a9311c8ec8cc5c411fc6f78
Tomachi [ICHIGO] <lego@aol.jp>
2021-05-24 04:38:20 +0900

    fix: #50

--- src/main/java/com/jaoafa/jdavcspeaker/Lib/MsgFormatter.java
+++ src/main/java/com/jaoafa/jdavcspeaker/Lib/MsgFormatter.java
@@ -4,21 +4,25 @@ import com.jaoafa.jdavcspeaker.StaticData;
 import com.vdurmont.emoji.EmojiParser;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class MsgFormatter {
     static Pattern parenthesesPattern = Pattern.compile("\\(.+\\)");
 
     public static String format(String text) {
-        //LengthCheck
+        // LengthCheck
         if (text.length() >= 180) {
             text = text.substring(0, 180);
         }
-        //EmojiCheck
+
+        // ReplaceUnicodeEmoji
         text = EmojiParser.parseToAliases(text);
 
-        //CustomEmojiCheck
+        // ReplaceCustomEmoji
         String regex = "<a?:(.+?):([0-9]+)>";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(text);
@@ -26,9 +30,19 @@ public class MsgFormatter {
             text = text.replace(m.group(), ":" + m.group(1) + ":");
         }
 
+        // RemoveParams
+        text = Arrays
+            .stream(text.split(" "))
+            .filter(s -> s.startsWith("speaker:"))
+            .filter(s -> s.startsWith("speed:"))
+            .filter(s -> s.startsWith("emotion:"))
+            .filter(s -> s.startsWith("emotion_level:"))
+            .filter(s -> s.startsWith("pitch:"))
+            .collect(Collectors.joining(" "));
+
         String[] splitText = text.split(" ");
 
-        //URLCheck
+        // URLCheck
         for (String s : splitText) {
             if (s.startsWith("https://") || s.startsWith("http://")) {
                 String[] urlSplit = s.split("/");
@@ -36,9 +50,11 @@ public class MsgFormatter {
             }
         }
 
-        final String[] formatText = {text};
-        StaticData.aliasMap.forEach((k, v) -> formatText[0] = formatText[0].replace(k, v));
-        return formatText[0];
+        // Alias
+        for (Map.Entry<String, String> entry : StaticData.aliasMap.entrySet()) {
+            text = text.replace(entry.getKey(), entry.getValue());
+        }
+        return text;
     }
 
     public static String formatChannelName(VoiceChannel channel) {
