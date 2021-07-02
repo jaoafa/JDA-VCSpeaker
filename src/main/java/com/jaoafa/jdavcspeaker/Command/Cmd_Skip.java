c0fccedf24734982f7036688c7b4ae4cd4009824
Tomachi [ICHIGO] <lego@aol.jp>
2021-07-02 22:40:22 +0900

    feat: #64 skipコマンドの実装

--- src/main/java/com/jaoafa/jdavcspeaker/Command/Cmd_Skip.java
+++ src/main/java/com/jaoafa/jdavcspeaker/Command/Cmd_Skip.java
@@ -15,23 +15,22 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 
 import static com.jaoafa.jdavcspeaker.Command.CmdExecutor.execute;
 
-public class Cmd_Clear implements CmdInterface {
+public class Cmd_Skip implements CmdInterface {
     @Override
     public CmdBuilders register(Command.Builder<JDACommandSender> builder) {
         return new CmdBuilders(
-                builder
-                        .handler(context -> execute(context, this::clear))
-                        .build()
+            builder
+                .handler(context -> execute(context, this::skip))
+                .build()
         );
     }
 
-    void clear(Guild guild, MessageChannel channel, Member member, Message message, CommandContext<JDACommandSender> context) {
-        PlayerManager.getINSTANCE().getGuildMusicManager(guild).scheduler.queue.clear();
-        PlayerManager.getINSTANCE().getGuildMusicManager(guild).player.destroy();
+    void skip(Guild guild, MessageChannel channel, Member member, Message message, CommandContext<JDACommandSender> context) {
+        PlayerManager.getINSTANCE().getGuildMusicManager(guild).scheduler.nextTrack();
         message.reply(new EmbedBuilder()
-            .setTitle(":stop_button: 読み上げをクリアしました！")
-                .setColor(LibEmbedColor.success)
-                .build()
+            .setTitle(":track_next: 読み上げをスキップします。")
+            .setColor(LibEmbedColor.success)
+            .build()
         ).queue();
     }
 }
