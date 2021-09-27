25fb7d04b3dff0b89a4316380e348938235e4ebe
Tomachi [ICHIGO] <lego@aol.jp>
2021-09-28 01:27:37 +0900

    feat: #21 エイリアスを正規表現で処理するように

--- src/main/java/com/jaoafa/jdavcspeaker/Lib/LibAlias.java
+++ src/main/java/com/jaoafa/jdavcspeaker/Lib/LibAlias.java
@@ -6,11 +6,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class LibAlias {
+    private static final Map<String, String> aliases = new HashMap<>();
+
     public static void fetchMap() {
         LibFlow aliasFlow = new LibFlow("LibAlias");
         File aliasConfig = new File("./alias.json");
@@ -25,25 +28,25 @@ public class LibAlias {
             }
         }
 
-        LibValue.aliasMap.clear();
+        aliases.clear();
         try {
             List<String> lines = Files.readAllLines(Paths.get("alias.json"));
             JSONObject obj = new JSONObject(String.join("\n", lines));
 
             for (Iterator<String> it = obj.keys(); it.hasNext(); ) {
                 String key = it.next();
-                LibValue.aliasMap.put(key, obj.getString(key));
+                aliases.put(key, obj.getString(key));
             }
-            aliasFlow.success("エイリアス設定を " + LibValue.aliasMap.size() + " 件ロードしました。");
+            aliasFlow.success("エイリアス設定を " + aliases.size() + " 件ロードしました。");
         } catch (IOException e) {
-            aliasFlow.error("除外設定のロード中にエラーが発生しました。");
+            aliasFlow.error("エイリアス設定のロード中にエラーが発生しました。");
             e.printStackTrace();
         }
     }
 
     public static void fetchJson() {
         JSONObject obj = new JSONObject();
-        LibValue.aliasMap.forEach(obj::put);
+        aliases.forEach(obj::put);
         try {
             Files.write(Paths.get("alias.json"), Collections.singleton(obj.toString()));
         } catch (IOException e) {
@@ -52,12 +55,27 @@ public class LibAlias {
     }
 
     public static void addToAlias(String value1, String value2) {
-        LibValue.aliasMap.put(value1, value2);
+        aliases.put(value1, value2);
         fetchJson();
     }
 
     public static void removeFromAlias(String value) {
-        LibValue.aliasMap.remove(value);
+        aliases.remove(value);
         fetchJson();
+    }
+
+    public static Map<String, String> getAliases() {
+        return aliases;
+    }
+
+    public static String applyAlias(String text) {
+        for (Map.Entry<String, String> entry : aliases.entrySet().stream().sorted(Comparator.comparingInt(e -> e.getKey().length())).collect(Collectors.toList())) {
+            Pattern pattern = Pattern.compile(entry.getKey());
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                text = matcher.replaceAll(entry.getValue());
+            }
+        }
+        return text;
     }
 }
