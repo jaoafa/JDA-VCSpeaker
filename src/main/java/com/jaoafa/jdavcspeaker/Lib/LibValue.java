67a80293673ce7b03ef5c0f8d6ce97639efde2f9
Tomachi [ICHIGO] <lego@aol.jp>
2021-10-10 03:11:31 +0900

    feat: rollbarでエラー収集

--- src/main/java/com/jaoafa/jdavcspeaker/Lib/LibValue.java
+++ src/main/java/com/jaoafa/jdavcspeaker/Lib/LibValue.java
@@ -1,6 +1,7 @@
 package com.jaoafa.jdavcspeaker.Lib;
 
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import com.rollbar.notifier.Rollbar;
 import net.dv8tion.jda.api.JDA;
 
 import java.util.ArrayList;
@@ -11,4 +12,5 @@ public class LibValue {
     public static final List<String> ignoreEquals = new ArrayList<>();
     public static JDA jda;
     public static EventWaiter eventWaiter = null;
+    public static Rollbar rollbar = null;
 }
